96c7c4589773891179f37a801332d45b85daed32
Blake Baldassaro <bbaldassaro@dolphindebit.com>
2025-12-30 16:11:56 -0600

    Testing webcam function

--- sketch.js
+++ sketch.js
@@ -1,75 +1,113 @@
-let x;
-let y;
-let t;
-let penPicker;
-let framePicker;
-function setup() {
-  createCanvas(2000, 2000);
-  translate(1000,1000)
-  rotate(PI)
-  background(0);
-  penPicker = createColorPicker('deeppink');
-  penPicker.position(50, 100);
-  penPicker.size(100, 100)
-  stroke(penPicker.color());
-  
-  noFill();
-  
-  framePicker = createColorPicker('blue')
-  framePicker.position(250, 100)
-  framePicker.size(100, 100)
-  drawFrame();
+  let x;
+  let y;
+  let t;
+  let penSettings;
+  let frameSettings;
+  let penColor;
+  let frameColor
+  let frameSize
+  let capture
 
-}
+  class Settings {
+ 
+    constructor(x, y, name) {
+      this.frame = createDiv(name)
+      this.x = x
+      this.y = y
+      this.frame.position(x,y)
+      this.frame.size(400,400)
+      this.frame.style('font-size', '16px');
+      this.frame.style('font-family', 'Comic Sans MS');
+      this.frame.style('background', 'blue');
+      this.frame.style('padding', '5px');
+      this.frame.draggable();
+    }
 
-function drawFrame(){
-  //draw frame
-  stroke(framePicker.color())
-  for(let fx = -450; fx < 450; fx++){
-    circle(fx, -450, 30)
+    addElement(elem, lx, ly){
+      this.frame.child(elem)
+      elem.position(x + lx, y + ly)
+    }
   }
-  for(let fx = -450; fx < 450; fx++){
-    circle(450, fx, 30)
+  function setup() {
+    createCanvas(2000, 2000)
+    translate(1000,1000)
+    rotate(PI)
+    background(0);
+
+    penSettings = new Settings(800, 300, 'Pen Settings')
+    penColor = createColorPicker('deeppink')
+    penColor.size(100,100)
+    penSettings.addElement(penColor, 20, 20)
+    
+
+    frameSettings = new Settings(300,300, 'Frame Settings')
+    frameColor = createColorPicker('green')
+    frameColor.size(100,100)
+    frameSettings.addElement(frameColor, 20, 200)
+
+    frameSize = createInput()
+    frameSettings.addElement(frameSize, 10, 200)
+
+    capture = createCapture(VIDEO, {flipped:true})
+    capture.size(2000, 2000)
+    capture.hide()
+
+    noFill();
+    drawFrame();
+    
+
   }
-  for(let fx = 450; fx > -450; fx--){
-    circle(fx, 450, 30)
+
+  function drawFrame(){
+    //draw frame
+    stroke(frameColor.color())
+    for(let fx = -450; fx < 450; fx++){
+      circle(fx, -450, 30)
+    }
+    for(let fx = -450; fx < 450; fx++){
+      circle(450, fx, 30)
+    }
+    for(let fx = 450; fx > -450; fx--){
+      circle(fx, 450, 30)
+    }
+    for(let fx = 450; fx > -450; fx--){
+      circle(-450, fx, 30)
+    }
+    
   }
-  for(let fx = 450; fx > -450; fx--){
-    circle(-450, fx, 30)
+  function draw() {
+    //translate origin to center
+    translate(1000,1000)
+    rotate(PI)
+    frameColor.changed(drawFrame)
+    stroke(penColor.color());
+    
+    image(capture, 0,0,width, height)
+
+    circle(0,0,10)
+    //draw heart
+    t = frameCount *.05 % 360;
+    x = 16*pow(sin(t), 3);
+    y = 13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
+    circle(6*x,6*y, 10);   
+
+
+    //draw frame
+    // circle(fx, fy, 10);
+    
+
+    // if(fx == -500 && fy < 500){
+    //   fy += 1;
+    // }
+    // else if(fx < 500 && fy == 500){
+    //   fx += 1;
+    // }
+    // else if(fx == 500 && fy > -500){
+    //   fy -= 1;
+    // }
+    // else if(fx > -500 && fy == -500){
+    //   fx -= 1;
+    // }
+
+
   }
-  
-}
-function draw() {
-  //translate origin to center
-  translate(1000,1000)
-  rotate(PI)
-  drawFrame()
-  stroke(penPicker.color());
-  
-  circle(0,0,10)
-  //draw heart
-  t = frameCount *.05 % 360;
-  x = 16*pow(sin(t), 3);
-  y = 13*cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t);
-  circle(6*x,6*y, 10);   
-
-
-  //draw frame
-  // circle(fx, fy, 10);
-  
-
-  // if(fx == -500 && fy < 500){
-  //   fy += 1;
-  // }
-  // else if(fx < 500 && fy == 500){
-  //   fx += 1;
-  // }
-  // else if(fx == 500 && fy > -500){
-  //   fy -= 1;
-  // }
-  // else if(fx > -500 && fy == -500){
-  //   fx -= 1;
-  // }
-
-
-}
